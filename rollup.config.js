f924b540de6be31a229cc16cdb9ac4b518d7a80a
hs <tlsgustn93@naver.com>
2024-10-26 17:31:28 +0900

    fix: esm, cjm es5,es6 대응

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,57 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { babel } from "@rollup/plugin-babel";
-import { terser } from "rollup-plugin-terser";
+import { terser } from "@rollup-plugin-terser";
 
-export default {
+const es5Config = {
   input: "src/index.ts",
   output: [
     {
-      file: "dist/index.cjs.js",
+      file: "dist/es5/index.cjs.js",
       format: "cjs",
       sourcemap: true,
     },
     {
-      file: "dist/index.esm.js",
+      file: "dist/es5/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: "./tsconfig.es5.json",
+      declaration: true,
+    }),
+    babel({
+      babelHelpers: "bundled",
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: "> 0.25%, not dead, ie 11",
+            modules: false,
+          },
+        ],
+      ],
+      extensions: [".js", ".ts"],
+      exclude: "node_modules/**",
+    }),
+    terser(),
+  ],
+};
+
+const es6Config = {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/es6/index.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/es6/index.esm.js",
       format: "esm",
       sourcemap: true,
     },
@@ -25,14 +64,8 @@ export default {
       tsconfig: "./tsconfig.json",
       declaration: true,
     }),
-    babel({
-      babelHelpers: "bundled",
-      presets: [
-        ["@babel/preset-env", { targets: "> 0.25%, not dead", modules: false }],
-      ],
-      extensions: [".js", ".ts"],
-      exclude: "node_modules/**",
-    }),
     terser(),
   ],
 };
+
+export default [es5Config, es6Config];
